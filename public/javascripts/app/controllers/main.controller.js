763a859e576b3d939de513d4b09be1efa8a841bd
Carlos Mendoza <carlos@Carloss-MacBook-Pro.local>
2015-07-16 09:29:01 -0400

    Refactored some code.

--- public/javascripts/app/controllers/main.controller.js
+++ public/javascripts/app/controllers/main.controller.js
@@ -2,55 +2,47 @@
 
 var app = angular.module('ngMyApp', []);
 
-app.controller('mainController', ['$scope', 'UsersFactory', 'PostsFactory', 'CommentsFactory', function($scope, UsersFactory, PostsFactory, CommentsFactory){
+app.controller('mainController', ['$scope', 'Users', 'Posts', 'Comments', function($scope, Users, Posts, Comments){
 		
-		$scope.title = "Sample Social Media Application Using Angular and Express!";
-
-		//Friends List Section
-
-		UsersFactory.getUsers()
+		$scope.title = 'Sample Social Media Application Using Angular and Express!';
+			
+		//gets all users
+		Users.get()
 			.then(function(data){
 				$scope.users = data;
-			})
-
-		PostsFactory.get()
-			.then(function(data){
-				$scope.posts = data;
-				console.log('POSTS', data);
 			});
 
+		//message displayed in profile/posts section when page is first loaded
+		$scope.welcome_message = true;
 
-		CommentsFactory.get()
-			.then(function(data){
-				console.log('COMMENTS', data);
-			});
-
-		$scope.welcomeMessage = true;
-
+		//gets user's posts when view profile button is clicked
 		$scope.toggleProfile = function(user) {
-			$scope.profilePosts = false;
-			$scope.profileDetails = true;
-			$scope.userDetails = user;
-			$scope.welcomeMessage = false;
+			$scope.profile_posts = false;
+			$scope.profile_details = true;
+			$scope.user_details = user;
+			$scope.welcome_message = false;
 
-			PostsFactory.getByUserId(user.id)
+			Posts.getByUserId(user.id)
 				.then(function(data){
-					$scope.userPosts = data;
+					$scope.user_posts = data;
 				})
 		} 
 
+		//profile button logic
 		$scope.showUserProfile = function() {
-			$scope.profilePosts = false;
-			$scope.profileDetails = true;
+			$scope.profile_posts = false;
+			$scope.profile_details = true;
 		}
 
+		//posts button logic
 		$scope.showUserPosts = function() {
-			$scope.profileDetails = false;
-			$scope.profilePosts = true;
+			$scope.profile_details = false;
+			$scope.profile_posts = true;
 		}
 
-		$scope.togglePostComments = function(postId, post, indx) {
-			CommentsFactory.getByPostId(postId)
+		//gets post's comments when see comments button is clicked
+		$scope.togglePostComments = function(post) {
+			Comments.getByPostId(post.id)
 				.then(function(data){
 					post.comments = data;
 				})
--- public/javascripts/app/factories/main.factory.js
+++ public/javascripts/app/factories/main.factory.js
@@ -1,6 +1,6 @@
 'use strict';
 
-app.factory('APIConfigFactory', [function() {
+app.factory('APIConfig', [function() {
 	
 	return {
 		devRoot: 'http://jsonplaceholder.typicode.com',
@@ -19,11 +19,12 @@ app.factory('APIConfigFactory', [function() {
 	}
 }]);
 
-app.factory('UsersFactory', ['$http', 'APIConfigFactory', function($http, APIConfigFactory){
+app.factory('Users', ['$http', 'APIConfig', function($http, APIConfig){
 	
-	return {		
-		getUsers: function() {
-			return $http.get(APIConfigFactory.getRoot() + '/users')
+	return {
+		//gets all users		
+		get: function() {
+			return $http.get(APIConfig.getRoot() + '/users')
 					.then(function(response){
 						return response.data;
 					})
@@ -31,18 +32,20 @@ app.factory('UsersFactory', ['$http', 'APIConfigFactory', function($http, APICon
 	};	
 }]);
 
-app.factory('PostsFactory', ['$http', 'APIConfigFactory', function($http, APIConfigFactory){
+app.factory('Posts', ['$http', 'APIConfig', function($http, APIConfig){
 	
 	return {
+		//gets all posts
 		get: function() {
-			return $http.get(APIConfigFactory.getRoot() + '/posts')
+			return $http.get(APIConfig.getRoot() + '/posts')
 					.then(function(response){
 						return response.data;
 					});
 		},
+		//gets user's posts using user.id
 		getByUserId: function(userId) {
 			console.log('id', userId)
-			return $http.get(APIConfigFactory.getRoot() + '/posts?userId=' + userId)
+			return $http.get(APIConfig.getRoot() + '/posts?userId=' + userId)
 					.then(function(response){
 						console.log('fad',response.data)
 						return response.data;
@@ -51,17 +54,19 @@ app.factory('PostsFactory', ['$http', 'APIConfigFactory', function($http, APICon
 	};
 }]);
 
-app.factory('CommentsFactory', ['$http', 'APIConfigFactory', function($http, APIConfigFactory){
+app.factory('Comments', ['$http', 'APIConfig', function($http, APIConfig){
 	
 	return {
+		//gets all comments
 		get: function() {
-			return $http.get(APIConfigFactory.getRoot() + '/comments/')
+			return $http.get(APIConfig.getRoot() + '/comments/')
 					.then(function(response){
 						return response.data;
 					})
 		},
+		//gets post's comments using post.id
 		getByPostId: function(postId) {
-			return $http.get(APIConfigFactory.getRoot() + '/comments?postId=' + postId)
+			return $http.get(APIConfig.getRoot() + '/comments?postId=' + postId)
 					.then(function(response){
 						return response.data;
 					})			
