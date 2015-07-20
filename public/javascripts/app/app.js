ce91ba602ac2a7090888bb90f9c9eccee0d1a48a
Carlos Mendoza <carlos@Carloss-MacBook-Pro.local>
2015-07-20 11:36:41 -0400

    switching to ng-include.

--- public/javascripts/app/app.js
+++ public/javascripts/app/app.js
@@ -11,16 +11,17 @@ app.config(function ($locationProvider, $urlRouterProvider) {
 app.config(function($stateProvider) {
 
 	$stateProvider
+		.state('main', {
+			url: '/',
+			templateUrl: '/views/index.html',
+			controller: 'MainController'
+		});
+
+	$stateProvider
 		.state('posts', {
 			url: '/posts-feed',
 			templateUrl: '/views/posts_feed.html',
 			controller: 'MainController'
 		});
 
-	$stateProvider
-		.state('main', {
-			url: '/',
-			templateUrl: '/views/index.html',
-			controller: 'MainController'
-		});
 });
