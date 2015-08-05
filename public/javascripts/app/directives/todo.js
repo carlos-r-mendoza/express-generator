415ec1f810cfc13b3ee00ec41822b4d419fa7b30
Carlos Mendoza <carlos@Carloss-MacBook-Pro.local>
2015-08-04 23:41:35 -0400

    Data is being passed to custom-table directive.

--- public/javascripts/app/directives/todo.js
+++ public/javascripts/app/directives/todo.js
@@ -11,10 +11,32 @@ app.directive('customTable', function($timeout) {
 		},
 		// link runs after directive has been compiled and linked up
 		link: function(scope, element, attrs) {
+			scope.rows = [];
+
+
 			// once values change in parent controller, update 
 			scope.$watch('data', function(newVal, oldVal) {
-			console.log('attrs', scope.type, scope.data, scope.headers)
-			
+
+				console.log('attrs', scope.type, scope.data, scope.headers)
+				
+				angular.forEach(scope.data, function(task){
+					var row = {
+						column1: "",
+						column2: "",
+						column3: "",
+						column4: ""
+						};
+
+					row.column1 = task.title;
+					row.column2 = "<h1>hello</h1>";
+					row.column3 = task.completed;
+					row.column4 = task.completed;
+
+					scope.rows.push(row);
+
+				});
+
+				console.log('rows', scope.rows)
 
 			})
 		}
